19e4507102f23e8c17cf2acc735aa772edf26710
CHEN Xian-an <xianan.chen@gmail.com>
2023-05-21 20:47:26 +0800

    use singleton

--- NCIAPHelper.h
+++ NCIAPHelper.h
@@ -2,25 +2,35 @@
 //  NCIAPHelper.h
 //  NCIAPHelper
 //
-//  Copyright (c) 2013 CHEN Xian'an <xianan.chen@gmail.com>. All rights reserved.
+//  Copyright (c) 2013 CHEN Xian'an <xianan.chen@gmail.com>. All rights
+//  reserved.
 //
-//  NCIAPHelper is available under the MIT license. you can use it into your app royalty-freely, just make sure that you don’t remove above copyright notice.
+//  NCIAPHelper is available under the MIT license. you can use it into your app
+//  royalty-freely, just make sure that you don’t remove above copyright notice.
 
 #import <Foundation/Foundation.h>
 #import <StoreKit/StoreKit.h>
 
-extern NSString * const kNCIAPHelperErrorDomain;
-typedef void (^NCIAPHelperCompletionHandler)(NSArray *paymentTransactions);
-typedef void (^NCIAPHelperErrorHandler)(NSArray *paymentTransactions, NSError *error);
+extern NSString* const kNCIAPHelperErrorDomain;
+typedef void (^NCIAPHelperCompletionHandler)(NSArray* paymentTransactions);
+typedef void (^NCIAPHelperErrorHandler)(
+    NSArray* paymentTransactions, NSError* error);
 
-typedef NS_ENUM(NSInteger, NCIAPHelperErrorCode){
+typedef NS_ENUM(NSInteger, NCIAPHelperErrorCode) {
   NCIAPHelperRequestContainsNoProductError = 0,
   NCIAPHelperInvalidProductIDError,
 };
 
 @interface NCIAPHelper : NSObject
 
-+ (void)purchaseProductWithID:(NSString *)productID completionHandler:(NCIAPHelperCompletionHandler)completionHandler errorHandler:(NCIAPHelperErrorHandler)errorHandler;
-+ (void)restoreCompletedTransactionsWithCompletionHandler:(NCIAPHelperCompletionHandler)completionHandler errorHandler:(NCIAPHelperErrorHandler)errorHandler;
++ (void)purchaseProductWithID:(NSString*)productID
+                  aboveWindow:(UIWindow*)window
+            completionHandler:(NCIAPHelperCompletionHandler)completionHandler
+                 errorHandler:(NCIAPHelperErrorHandler)errorHandler;
++ (void)restoreCompletedTransactionsAboveWindow:(UIWindow*)window
+                              completionHandler:(NCIAPHelperCompletionHandler)
+                                                    completionHandler
+                                   errorHandler:
+                                       (NCIAPHelperErrorHandler)errorHandler;
 
 @end
--- NCIAPHelper.m
+++ NCIAPHelper.m
@@ -2,181 +2,205 @@
 //  NCIAPHelper.m
 //  NCIAPHelper
 //
-//  Copyright (c) 2013 CHEN Xian'an <xianan.chen@gmail.com>. All rights reserved.
+//  Copyright (c) 2013 CHEN Xian'an <xianan.chen@gmail.com>. All rights
+//  reserved.
 //
-//  NCIAPHelper is available under the MIT license. you can use it into your app royalty-freely, just make sure that you don’t remove above copyright notice.
+//  NCIAPHelper is available under the MIT license. you can use it into your app
+//  royalty-freely, just make sure that you don’t remove above copyright notice.
 
 #import "NCIAPHelper.h"
+#import <UIKit/UIKit.h>
 #import <objc/runtime.h>
-#import <CXAFoundation/CXAAlertController.h>
 
-#define SELF                      [self self]
+NSString* const kNCIAPHelperErrorDomain = @"kNCIAPHelperErrorDomain";
 
-NSString * const kNCIAPHelperErrorDomain = @"kNCIAPHelperErrorDomain";
+@interface NCIAPHelper () <SKProductsRequestDelegate,
+    SKPaymentTransactionObserver>
 
-static CXAAlertController *alertController;
-
-@interface NCIAPHelper()
-
-+ (NCIAPHelperCompletionHandler)completionHandler;
-+ (void)setCompletionHandler:(NCIAPHelperCompletionHandler)completionHandler;
-+ (NCIAPHelperErrorHandler)errorHandler;
-+ (void)setErrorHandler:(NCIAPHelperErrorHandler)errorHandler;
+@property (class, readonly, strong) NCIAPHelper* shared;
+@property (nonatomic, strong) UIAlertController* alertController;
+@property (nonatomic, weak) UIWindow* aboveWindow;
+@property (nonatomic, strong) NCIAPHelperCompletionHandler completionHandler;
+@property (nonatomic, strong) NCIAPHelperErrorHandler errorHandler;
 
 @end
 
 @implementation NCIAPHelper
 
-+ (void)load
++ (instancetype)shared
 {
   static dispatch_once_t onceToken;
-  dispatch_once(&onceToken, ^{
-    [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidFinishLaunchingNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *note){
-      [[SKPaymentQueue defaultQueue] addTransactionObserver:SELF];
-    }];
-  });
+  static id shared;
+  dispatch_once(&onceToken, ^{ shared = [[self alloc] init]; });
+  return shared;
 }
 
-+ (void)purchaseProductWithID:(NSString *)productID
+- (instancetype)init
+{
+  if (!(self = [super init])) return nil;
+  [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
+  return self;
+}
+
++ (void)purchaseProductWithID:(NSString*)productID
+                  aboveWindow:(UIWindow*)window
             completionHandler:(NCIAPHelperCompletionHandler)completionHandler
                  errorHandler:(NCIAPHelperErrorHandler)errorHandler
 {
-  if (![SKPaymentQueue canMakePayments]){
-    [CXAAlertController showAlertWithTitle:NSLocalizedString(@"In App Purchase Disabled", nil) message:NSLocalizedString(@"Sorry, In App Purchase is disabled on this device. You might need to ask the owner for help or enable it youself in Settings.app.", nil) cancelTitle:NSLocalizedString(@"Goodbye", nil) inViewController:[UIApplication sharedApplication].delegate.window.rootViewController];
+  self.shared.aboveWindow = window;
+  if (![SKPaymentQueue canMakePayments]) {
+    NSString* title = NSLocalizedString(@"In App Purchase Disabled", nil);
+    NSString* message = NSLocalizedString(
+        @"Sorry, In App Purchase is disabled on this device. You might need to "
+        @"ask the owner for help or enable it youself in Settings.app.",
+        nil);
+    UIAlertController* alert = [UIAlertController
+        alertControllerWithTitle:title
+                         message:message
+                  preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction
+                         actionWithTitle:NSLocalizedString(@"Goodbye", nil)
+                                   style:UIAlertActionStyleCancel
+                                 handler:^(UIAlertAction* _Nonnull action) {}]];
+    [window.rootViewController presentViewController:alert animated:YES
+                                          completion:nil];
     return;
   }
-  
-  [self setCompletionHandler:completionHandler];
-  [self setErrorHandler:errorHandler];
-  alertController = [CXAAlertController showAlertWithTitle:NSLocalizedString(@"Connecting to Apple…", nil) message:nil cancelTitle:nil inViewController:[UIApplication sharedApplication].delegate.window.rootViewController];
-  SKProductsRequest *request= [[SKProductsRequest alloc] initWithProductIdentifiers:[NSSet setWithObject:productID]];
-  request.delegate = SELF;
+
+  self.shared.completionHandler = completionHandler;
+  self.shared.errorHandler = errorHandler;
+  self.shared.alertController = [UIAlertController
+      alertControllerWithTitle:NSLocalizedString(@"Connecting to Apple…", nil)
+                       message:nil
+                preferredStyle:UIAlertControllerStyleAlert];
+  [window.rootViewController presentViewController:self.shared.alertController
+                                          animated:YES
+                                        completion:nil];
+  SKProductsRequest* request = [[SKProductsRequest alloc]
+      initWithProductIdentifiers:[NSSet setWithObject:productID]];
+  request.delegate = self.shared;
   [request start];
 }
 
-+ (void)restoreCompletedTransactionsWithCompletionHandler:(NCIAPHelperCompletionHandler)completionHandler
-                                             errorHandler:(NCIAPHelperErrorHandler)errorHandler
++ (void)restoreCompletedTransactionsAboveWindow:(UIWindow*)window
+                              completionHandler:(NCIAPHelperCompletionHandler)
+                                                    completionHandler
+                                   errorHandler:
+                                       (NCIAPHelperErrorHandler)errorHandler
 {
-  alertController = [CXAAlertController showAlertWithTitle:NSLocalizedString(@"Restoring…", nil) message:nil cancelTitle:nil inViewController:[UIApplication sharedApplication].delegate.window.rootViewController];
+  self.shared.alertController = [UIAlertController
+      alertControllerWithTitle:NSLocalizedString(@"Restoring…", nil)
+                       message:nil
+                preferredStyle:UIAlertControllerStyleAlert];
+  [window.rootViewController presentViewController:self.shared.alertController
+                                          animated:YES
+                                        completion:nil];
   [[SKPaymentQueue defaultQueue] restoreCompletedTransactions];
-  [self setCompletionHandler:completionHandler];
-  [self setErrorHandler:errorHandler];
+  self.shared.completionHandler = completionHandler;
+  self.shared.errorHandler = errorHandler;
 }
 
 #pragma mark - SKProductsRequestDelegate
-+ (void)productsRequest:(SKProductsRequest *)request
-     didReceiveResponse:(SKProductsResponse *)response
+- (void)productsRequest:(SKProductsRequest*)request
+     didReceiveResponse:(SKProductsResponse*)response
 {
-  [alertController dismiss];
-  SKProduct *p = [response.products lastObject];
-  if (!p && [self errorHandler]){
-    NSString *invalidID = [response.invalidProductIdentifiers lastObject];
-    NSInteger code = invalidID ? NCIAPHelperInvalidProductIDError : NCIAPHelperRequestContainsNoProductError;
-    NSString *desc = invalidID ? [NSString stringWithFormat:NSLocalizedString(@"The purchase to the product ID “%@” is invalid.", nil), invalidID] : NSLocalizedString(@"Fail to get product information.", nil);
-    NSError *error = [NSError errorWithDomain:kNCIAPHelperErrorDomain code:code userInfo:@{NSLocalizedDescriptionKey : desc}];
-    [self errorHandler](nil, error);
-    [self setCompletionHandler:nil];
-    [self setErrorHandler:nil];
+  dispatch_async(dispatch_get_main_queue(), ^{
+    [self.alertController dismissViewControllerAnimated:YES completion:nil];
+  });
+  SKProduct* p = [response.products lastObject];
+  if (!p && [self errorHandler]) {
+    NSString* invalidID = [response.invalidProductIdentifiers lastObject];
+    NSInteger code = invalidID ? NCIAPHelperInvalidProductIDError
+                               : NCIAPHelperRequestContainsNoProductError;
+    NSString* desc = invalidID
+        ? [NSString
+            stringWithFormat:
+                NSLocalizedString(
+                    @"The purchase to the product ID “%@” is invalid.", nil),
+            invalidID]
+        : NSLocalizedString(@"Fail to get product information.", nil);
+    NSError* error =
+        [NSError errorWithDomain:kNCIAPHelperErrorDomain code:code
+                        userInfo:@{ NSLocalizedDescriptionKey : desc }];
+    self.errorHandler(nil, error);
+    self.completionHandler = nil;
+    self.errorHandler = nil;
     return;
   }
-  
-  SKPayment *pm = [SKPayment paymentWithProduct:p];
+
+  SKPayment* pm = [SKPayment paymentWithProduct:p];
   [[SKPaymentQueue defaultQueue] addPayment:pm];
 }
 
-#pragma mark - SKPaymentTransactionObserver 
-+ (void)paymentQueue:(SKPaymentQueue *)queue
- updatedTransactions:(NSArray *)transactions
+#pragma mark - SKPaymentTransactionObserver
+- (BOOL)paymentQueue:(SKPaymentQueue*)queue
+    shouldAddStorePayment:(SKPayment*)payment
+               forProduct:(SKProduct*)product
 {
-  [alertController dismiss];
-  for (SKPaymentTransaction *t in transactions){
-    if (t.transactionState == SKPaymentTransactionStatePurchased){
+  return true;
+}
+
+- (void)paymentQueue:(SKPaymentQueue*)queue
+    updatedTransactions:(NSArray*)transactions
+{
+  [self.alertController dismissViewControllerAnimated:YES completion:nil];
+  for (SKPaymentTransaction* t in transactions) {
+    if (t.transactionState == SKPaymentTransactionStatePurchased) {
       [[SKPaymentQueue defaultQueue] finishTransaction:t];
       NCIAPHelperCompletionHandler compHandler = [self completionHandler];
-      if (compHandler){
-        compHandler(@[t]);
+      if (compHandler) {
+        compHandler(@[ t ]);
         [self setCompletionHandler:nil];
       }
-    } else if (t.transactionState == SKPaymentTransactionStateFailed){
+    } else if (t.transactionState == SKPaymentTransactionStateFailed) {
       [[SKPaymentQueue defaultQueue] finishTransaction:t];
-      if (t.error.code != SKErrorPaymentCancelled){
+      if (t.error.code != SKErrorPaymentCancelled) {
         NCIAPHelperErrorHandler errHandler = [self errorHandler];
-        if (errHandler){
-          errHandler(@[t], t.error);
+        if (errHandler) {
+          errHandler(@[ t ], t.error);
           [self setErrorHandler:nil];
         }
       }
-    } else if (t.transactionState == SKPaymentTransactionStatePurchasing){
-      alertController = [CXAAlertController showAlertWithTitle:NSLocalizedString(@"Preparing for Payment…", nil) message:nil cancelTitle:nil inViewController:[UIApplication sharedApplication].delegate.window.rootViewController];
-    } else if (t.transactionState == SKPaymentTransactionStateRestored){
-      // Do nothing, let it be handled with `+paymentQueueRestoreCompletedTransactionsFinished:`
+    } else if (t.transactionState == SKPaymentTransactionStatePurchasing) {
+      self.alertController = [UIAlertController
+          alertControllerWithTitle:NSLocalizedString(
+                                       @"Preparing for Payment…", nil)
+                           message:nil
+                    preferredStyle:UIAlertControllerStyleAlert];
+      [self.aboveWindow.rootViewController
+          presentViewController:self.alertController
+                       animated:YES
+                     completion:nil];
+    } else if (t.transactionState == SKPaymentTransactionStateRestored) {
+      // Do nothing, handled by
+      // `-paymentQueueRestoreCompletedTransactionsFinished:`
     }
   }
 }
 
-+ (void)paymentQueueRestoreCompletedTransactionsFinished:(SKPaymentQueue *)queue
+- (void)paymentQueueRestoreCompletedTransactionsFinished:(SKPaymentQueue*)queue
 {
-  [alertController dismiss];
+  [self.alertController dismissViewControllerAnimated:YES completion:nil];
   NCIAPHelperCompletionHandler compHandler = [self completionHandler];
-  // You should check the transactions if they are matching your desired product ids
-  if (compHandler)
-    compHandler(queue.transactions);
-  
-  for (SKPaymentTransaction *pt in queue.transactions)
+  // You should check the transactions if they are matching your desired product
+  // ids
+  if (compHandler) compHandler(queue.transactions);
+
+  for (SKPaymentTransaction* pt in queue.transactions)
     [[SKPaymentQueue defaultQueue] finishTransaction:pt];
-  
+
   [self setCompletionHandler:nil];
   [self setErrorHandler:nil];
 }
 
-+ (void)paymentQueue:(SKPaymentQueue *)queue
-restoreCompletedTransactionsFailedWithError:(NSError *)error
+- (void)paymentQueue:(SKPaymentQueue*)queue
+    restoreCompletedTransactionsFailedWithError:(NSError*)error
 {
-  [alertController dismiss];
-  NCIAPHelperErrorHandler errHandler = [self errorHandler];
-  if (errHandler)
-    errHandler(queue.transactions, error);
-  
-  [self setCompletionHandler:nil];
-  [self setErrorHandler:nil];
-}
+  [self.alertController dismissViewControllerAnimated:YES completion:nil];
+  if (self.errorHandler) self.errorHandler(queue.transactions, error);
 
-//#pragma mark - UIAlertViewDelegate
-//+ (void)willPresentAlertView:(UIAlertView *)alertView
-//{
-//  if (alertView.tag == TAG_REQUESTING_PRODUCT ||
-//      alertView.tag == TAG_PREPARING_FOR_PAYMENT ||
-//      alertView.tag == TAG_RESTORING){
-//    UIActivityIndicatorView *aiv = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
-//    aiv.center = CGPointMake(CGRectGetMidX(alertView.bounds), CGRectGetMidY(alertView.bounds) + 10);
-//    [aiv startAnimating];
-//    [alertView addSubview:aiv];
-//  }
-//}
-
-#pragma mark - privates
-
-static char kCompletionHandlerKey;
-static char kErrorHandlerKey;
-
-+ (NCIAPHelperCompletionHandler)completionHandler
-{
-  return objc_getAssociatedObject(SELF, &kCompletionHandlerKey);
-}
-
-+ (void)setCompletionHandler:(NCIAPHelperCompletionHandler)completionHandler
-{
-  objc_setAssociatedObject(SELF, &kCompletionHandlerKey, completionHandler, OBJC_ASSOCIATION_COPY_NONATOMIC);
-}
-
-+ (NCIAPHelperErrorHandler)errorHandler
-{
-  return objc_getAssociatedObject(SELF, &kErrorHandlerKey);
-}
-
-+ (void)setErrorHandler:(NCIAPHelperErrorHandler)errorHandler
-{
-  objc_setAssociatedObject(SELF, &kErrorHandlerKey, errorHandler, OBJC_ASSOCIATION_COPY_NONATOMIC);
+  self.completionHandler = nil;
+  self.errorHandler = nil;
 }
 
 @end
